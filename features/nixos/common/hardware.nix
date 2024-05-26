492e563db14d4d8d7b799be08033043754ace3a6
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-05-26 15:18:15 -0500

    enable opengl

--- features/nixos/common/hardware.nix
+++ features/nixos/common/hardware.nix
@@ -12,5 +12,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
 }
