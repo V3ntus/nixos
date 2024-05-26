04059a10333e657976a659e4b6584e428a7112c6
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-05-26 17:21:10 -0500

    add kitty

--- hosts/ventus-pc/home.nix
+++ hosts/ventus-pc/home.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../features/home-manager/base.nix
     ../../features/home-manager/desktop/hyprland.nix
+    ../../features/home-manager/terminal/kitty.nix
   ];
 
   home.username = "ventus";
