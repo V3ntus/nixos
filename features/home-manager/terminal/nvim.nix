e7db7c8d7c8b343b44219cb5fb2e63553e5b372b
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-05-27 14:17:40 -0500

    add lazy.nvim

--- features/home-manager/terminal/nvim.nix
+++ features/home-manager/terminal/nvim.nix
@@ -1,6 +1,6 @@
-{pkgs, ...}:
-let
+{pkgs, ...}: let
   toLua = str: "lua << EOF\n${str}\nEOF\n";
+  toLuaFromFile = path: "lua << EOF\n${builtins.readFile path}\nEOF\n";
 in {
   programs.neovim = {
     enable = true;
@@ -8,8 +8,14 @@ in {
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
-    
+
     plugins = with pkgs.vimPlugins; [
+      {
+        plugin = nvim-lspconfig;
+        config = toLua ''
+          require'lspconfig'.nil_ls.setup{}
+        '';
+      }
       {
         plugin = gruvbox-nvim;
         config = toLua ''
@@ -17,9 +23,21 @@ in {
           vim.cmd("colorscheme gruvbox")
         '';
       }
+      
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-json
+        ]));
+      }
     ];
 
     extraLuaConfig = ''
+      ${builtins.readFile ./nvim/plugin.lua}
       ${builtins.readFile ./nvim/options.lua}
     '';
   };
