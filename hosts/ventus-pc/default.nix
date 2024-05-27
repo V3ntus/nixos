b363f703ea048aded23d9e997665a0c4e911b4e3
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-05-27 13:46:13 -0500

    add sddm

--- hosts/ventus-pc/default.nix
+++ hosts/ventus-pc/default.nix
@@ -5,6 +5,7 @@
     ../../features/nixos/common
     #    ../../features/nixos/virtualization/guest.nix # todo remove this
     ../../features/nixos/desktop/hyprland.nix
+    ../../features/nixos/desktop/sddm.nix
 
     ../../users/joe.nix
   ];
