eeb71008722ffaa640423f1f9115751c24767ff0
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-05-28 22:58:41 -0500

    add XDG_DESKTOP env vars

--- features/home-manager/desktop/hyprland.nix
+++ features/home-manager/desktop/hyprland.nix
@@ -54,6 +54,8 @@
       env = [
         "XCURSOR_SIZE,24"
         "XDG_SESSION_TYPE,wayland"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_DESKTOP,Hyprland"
         "NIXOS_OZONE_WL,\"1\""
         "MOZ_ENABLE_WAYLAND,1"
         "WLR_NO_HARDWARE_CURSORS,1"
