6ae0fde5838678c4c475af2dbd89ec6fd9fba2df
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-06-06 09:49:09 -0500

    add wireguard config

--- hosts/joe-work/default.nix
+++ hosts/joe-work/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   inputs,
   ...
@@ -8,14 +9,31 @@
     ./nvidia.nix
 
     ../../features/nixos/common
+    ../../features/nixos/common/wireguard.nix
     ../../features/nixos/desktop/hyprland.nix
     ../../features/nixos/desktop/sddm.nix
 
     ../../users/joe.nix
   ];
 
+  sops.secrets = {
+    "wireguard/joe-work/privkey" = {
+      sopsFile = ../../users/secrets.yaml;
+    };
+  };
+
   users.users.joe.packages = [
     inputs.thorium.packages.x86_64-linux.thorium-avx
     pkgs.jetbrains.pycharm-professional
   ];
+
+  networking.wg-quick.interfaces.wg0.address = [
+    "10.143.245.4/24"
+    "fd11:5ee:bad:c0de::4/64"
+  ];
+  networking.wg-quick.interfaces.wg0.dns = [
+    "192.168.2.6"
+    "9.9.9.9"
+  ];
+  networking.wg-quick.interfaces.wg0.privateKeyFile = config.sops.secrets."wireguard/joe-work/privkey".path;
 }
