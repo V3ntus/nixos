2fc3083080116bfe4bfcc2a2a8a473045f878636
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-06-10 11:18:46 -0500

    i hate nvidia

--- hosts/joe-work/nvidia.nix
+++ hosts/joe-work/nvidia.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # Add NVIDIA package
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
 
   hardware.opengl = {
     enable = true;
@@ -20,14 +20,14 @@
     powerManagement.enable = false;
     powerManagement.finegrained = false;
 
-    open = true;
+    open = false;
 
     nvidiaSettings = true;
 
-    prime = {
-      sync.enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
+    #prime = {
+    #  sync.enable = true;
+    #  intelBusId = "PCI:0:2:0";
+    #  nvidiaBusId = "PCI:1:0:0";
+    #};
   };
 }
