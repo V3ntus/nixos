0b76bfbd74ce9cb26ad80a9fd52d79e52255635d
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-06-14 22:16:56 -0500

    add chili theme to sddm

--- features/nixos/desktop/sddm.nix
+++ features/nixos/desktop/sddm.nix
@@ -1,6 +1,11 @@
-{
+{pkgs, ...}: {
+  environment.systemPackages = [
+    pkgs.sddm-chili-theme
+  ];
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "chili";
+
   };
 }
