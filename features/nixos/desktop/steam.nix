f40de8767fdf3153552f48d98af0a11e03ceaf8e
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-07-06 23:53:17 -0500

    add hdr support to steam/gamescope

--- features/nixos/desktop/steam.nix
+++ features/nixos/desktop/steam.nix
@@ -1,11 +1,21 @@
-{
+{pkgs, ...}: {
+  environment.systemPackages = [
+    pkgs.mangohud
+  ];
+
   programs.gamescope = {
     enable = true;
+    args = [
+      "--hdr-enabled"
+    ];
   };
 
   programs.steam = {
     gamescopeSession = {
       enable = true;
+      args = [
+        "--hdr-enabled"
+      ];
     };
     enable = true;
   };
