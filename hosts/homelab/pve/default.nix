329eb071531cf06b9c4d399c2ae3896c690487db
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-07-10 20:58:07 -0500

    add ceph overlay (NixOS/nixpkgs#325059)

--- hosts/homelab/pve/default.nix
+++ hosts/homelab/pve/default.nix
@@ -7,6 +7,16 @@
 
     {
       nixpkgs.overlays = [
+        (self: super: {
+          # src: https://github.com/NixOS/nixpkgs/commit/7e94ac48e0c68bdc9d2b39e50e024e7170f83838
+          # issue/PR: https://github.com/NixOS/nixpkgs/pull/325059
+          ceph = super.ceph.overrideAttrs {
+            postPatch = ''
+              substituteInPlace cmake/modules/Finduring.cmake \
+                --replace-fail "liburing.a liburing" "uring"
+            '';
+          };
+        })
         proxmox-nixos.overlays.${system}
       ];
     }
