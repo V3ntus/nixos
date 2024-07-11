d88baeb585c3f643a522dcb0fec22a9463889fd4
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-07-11 13:40:21 -0500

    add srvos modules to nix lxc

--- hosts/homelab/nix/default.nix
+++ hosts/homelab/nix/default.nix
@@ -1,8 +1,12 @@
-{nixpkgs, sops-nix, ...}: nixpkgs.lib.nixosSystem {
+{nixpkgs, srvos, sops-nix, ...}: nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
 
   modules = [
     sops-nix.nixosModules.sops
+
+    srvos.nixosModules.server
+    srvos.nixosModules.mixins-trusted-nixos-caches
+    srvos.nixosModules.mixins-nix-experimental
 
     ./configuration.nix
   ];
