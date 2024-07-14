75695dde8f480c37151c45b0a0e5ffeb077619a4
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-07-14 14:11:35 -0500

    add ssh pubkey to root trusted keys

--- users/root.nix
+++ users/root.nix
@@ -16,6 +16,9 @@
     name = "root";
     isSystemUser = true;
     hashedPasswordFile = config.sops.secrets."users/root/password".path;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAduLznqkN0rvDysKrE2FQLegyeRzWyVu1Z71VPs2N7y root@nixos"
+    ];
     shell = pkgs.zsh;
   };
 }
