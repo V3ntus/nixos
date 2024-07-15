89f54b992e34443e4e2c4f5014b1ba04f91c2090
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-07-15 18:47:01 -0500

    add basic hardware config to baseline config

--- hosts/homelab/_base/configuration.nix
+++ hosts/homelab/_base/configuration.nix
@@ -1,6 +1,12 @@
-{
+{lib, modulesPath}: {
   # Baseline configuration for initial remote deployment.
   # This is manually uploaded to a new host and switched to.
+  imports = [
+    (modulesPath + "/virtualisation/proxmox-lxc.nix")
+    ./hardware-configuration.nix  # make sure to run nixos-generate-config to get the hardware config
+  ];
+
+  system.stateVersion = "24.11";
 
   programs.git.enable = true;
 
