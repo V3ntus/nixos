ee41f19c5641da145ba1026bce70b2f52fd5c37f
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-09-23 22:58:29 -0600

    add hostname to spaceship terminal

--- features/home-manager/terminal/ohmyzsh.nix
+++ features/home-manager/terminal/ohmyzsh.nix
@@ -103,7 +103,7 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "[](color_orange)$os$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)[](fg:color_blue bg:color_bg3)$docker_context$conda[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$c$rust$golang$nodejs$php$java$kotlin$haskell$python$nix_shell$character";
+      format = "[](color_orange)$os$username@$host[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)[](fg:color_blue bg:color_bg3)$docker_context$conda[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$c$rust$golang$nodejs$php$java$kotlin$haskell$python$nix_shell$character";
 
       palette = "gruvbox_dark";
 
