24129a68df65e1a041daba886d0a8f8bc505cd2e
V3ntus <29584664+V3ntus@users.noreply.github.com>
2025-04-30 22:39:21 +0000

    make security default priority

--- features/nixos/common/security.nix
+++ features/nixos/common/security.nix
@@ -1,17 +1,23 @@
-{
-  users.mutableUsers = true;
+{lib, ...}: {
+  users.mutableUsers = lib.mkDefault true;
 
-  programs.ssh.startAgent = true;
+  programs.ssh.startAgent = lib.mkDefault true;
 
-  security.pam.services = {hyprlock = {};};
+  security.pam.services = lib.mkDefault {hyprlock = {};};
 
   security.pki.certificates = [
     ''
-      CS30 Root CA
-      =========
-      -----BEGIN CERTIFICATE-----
-      MIIBjjCCATWgAwIBAgIQXBqd86r1ah+/GwIlvbOoFDAKBggqhkjOPQQDAjAmMQ0w\nCwYDVQQKEwRDUzMwMRUwEwYDVQQDEwxDUzMwIFJvb3QgQ0EwHhcNMjUwMzA2MTgy\nOTA2WhcNMzUwMzA0MTgyOTA2WjAmMQ0wCwYDVQQKEwRDUzMwMRUwEwYDVQQDEwxD\nUzMwIFJvb3QgQ0EwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARazvF58f0yORZN\n+h2GxOBryvMrVTBQwHTbD7b9jgYHcivAFI7fh6qr0y/OQ+FXirIGTcXBWCkkx3dA\nXlIDpGRfo0UwQzAOBgNVHQ8BAf8EBAMCAQYwEgYDVR0TAQH/BAgwBgEB/wIBATAd\nBgNVHQ4EFgQUB39IdWEdAiDJtv4hnB5KMyL+cbgwCgYIKoZIzj0EAwIDRwAwRAIg\nCyuSsit6gEuBJ+4XuExf54o3M0PqjZEIEKxXQMUFt/sCIGUTdLR+bEGOhi2qMIWS\nDfTza6xBAQibV+1Dii8UMtq3
-      -----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIBjjCCATWgAwIBAgIQXBqd86r1ah+/GwIlvbOoFDAKBggqhkjOPQQDAjAmMQ0w
+CwYDVQQKEwRDUzMwMRUwEwYDVQQDEwxDUzMwIFJvb3QgQ0EwHhcNMjUwMzA2MTgy
+OTA2WhcNMzUwMzA0MTgyOTA2WjAmMQ0wCwYDVQQKEwRDUzMwMRUwEwYDVQQDEwxD
+UzMwIFJvb3QgQ0EwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARazvF58f0yORZN
++h2GxOBryvMrVTBQwHTbD7b9jgYHcivAFI7fh6qr0y/OQ+FXirIGTcXBWCkkx3dA
+XlIDpGRfo0UwQzAOBgNVHQ8BAf8EBAMCAQYwEgYDVR0TAQH/BAgwBgEB/wIBATAd
+BgNVHQ4EFgQUB39IdWEdAiDJtv4hnB5KMyL+cbgwCgYIKoZIzj0EAwIDRwAwRAIg
+CyuSsit6gEuBJ+4XuExf54o3M0PqjZEIEKxXQMUFt/sCIGUTdLR+bEGOhi2qMIWS
+DfTza6xBAQibV+1Dii8UMtq3
+-----END CERTIFICATE-----
     ''
   ];
 }
