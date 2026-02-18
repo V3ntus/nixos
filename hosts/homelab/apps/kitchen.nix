6f440d23025d2c9efd0599b606017169372bfe14
V3ntus <29584664+V3ntus@users.noreply.github.com>
2026-02-18 02:09:25 +0000

    fix tandoor MEDIA_ROOT security advisory

--- hosts/homelab/apps/kitchen.nix
+++ hosts/homelab/apps/kitchen.nix
@@ -2,6 +2,9 @@
   services.tandoor-recipes = {
     enable = true;
     address = "0.0.0.0";
+    extraConfig = {
+      MEDIA_ROOT = "/var/lib/tandoor-recipes/media";
+    };
     port = 8001;
   };
 }
