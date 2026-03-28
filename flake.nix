7e2dbc0d2b206c053c40735c1263cf2d169bcd44
V3ntus <29584664+V3ntus@users.noreply.github.com>
2026-03-28 23:58:09 +0000

    remove ventus-pc for now

--- flake.nix
+++ flake.nix
@@ -84,40 +84,6 @@
 
     nixosConfigurations =
       {
-        # Gaming PC at home
-        ventus-pc = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = {inherit inputs;};
-          modules = [
-            ./hosts/ventus-pc
-            stylix.nixosModules.stylix
-            niri.nixosModules.niri
-            sops-nix.nixosModules.sops
-            comin.nixosModules.comin
-            ({...}: {
-              services.comin = {
-                enable = true;
-                remotes = [
-                  {
-                    name = "origin";
-                    url = gitHubRepo;
-                    branches.main.name = "main";
-                  }
-                ];
-              };
-            })
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit neovim apple-fonts swww waybar;
-              };
-              home-manager.users.joe = import ./hosts/ventus-pc/home.nix;
-            }
-          ];
-        };
-
         gladiusso = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = {inherit inputs;};
